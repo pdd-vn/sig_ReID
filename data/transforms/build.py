4b850964ba4ca885c1a860a8be70d6a7614724cc
dd-dos <dungphamdang99@gmail.com>
2020-11-11 00:10:07 +0700

    new commit

--- data/transforms/build.py
+++ data/transforms/build.py
@@ -7,25 +7,48 @@
 import torchvision.transforms as T
 import random
 import numpy as np
-
+import cv2
 
 from imgaug import augmenters as iaa
 from PIL import Image
 from torch.utils.data import Dataset
 
+def change_sig_color(img, color=None):
+    '''
+    change image color from black white
+    '''
+    if color is None:
+        color = [random.randint(0,255), random.randint(0,255), random.randint(0,255)]
 
-def custom_transform(img, prob=0.5):
+    if isinstance(img, Image.Image):
+        img = np.array(img)
+
+    if len(img.shape) != 3:
+        img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+    
+    sig_pos = np.where(img[:,:,0] != 255)
+    img[:,:,0][sig_pos] = color[0]
+    img[:,:,1][sig_pos] = color[1]
+    img[:,:,2][sig_pos] = color[2]
+
+    return Image.fromarray(img)
+
+
+def custom_transform(img, prob=0.7):
     if random.uniform(0, 1) >= prob:
         return img
 
-    img_array = np.array(img)
+    # change img color
+    img = change_sig_color(img)
 
+    # rotate and add noise
     aug = iaa.Sequential([
         iaa.Affine(rotate=(-10,10)),
         iaa.AdditiveGaussianNoise(scale=(10,30))
     ], random_order=True)
 
-    aug_img = aug(img_array)
+    img_array = np.array(img)
+    aug_img = aug(image=img_array)
 
     result = Image.fromarray(aug_img)
     
@@ -50,3 +73,9 @@ def build_transforms(cfg, is_train=True):
         ])
 
     return transform
+
+
+if __name__=="__main__":
+    img = Image.open("/home/pdd/Desktop/workspace/sig_ReID/raw_sig/2/NFI-01601033.png")
+    img = custom_transform(img, prob = 2)
+    img.show()
